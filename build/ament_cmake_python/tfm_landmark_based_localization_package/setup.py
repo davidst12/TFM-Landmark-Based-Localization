1ea2c1114f284d44e43dad9b3591860b99c28e3a
davidst12 <david.salto00@gmail.com>
2024-01-14 12:11:48 +0100

    remove build folder

--- build/ament_cmake_python/tfm_landmark_based_localization_package/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='tfm_landmark_based_localization_package',
-    version='0.0.0',
-    packages=find_packages(
-        include=('tfm_landmark_based_localization_package', 'tfm_landmark_based_localization_package.*')),
-)
--- build/libs/json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /hdd/ros2_tfm_ws/src/tfm_landmark_based_localization_package/libs/json
-# Build directory: /hdd/ros2_tfm_ws/src/tfm_landmark_based_localization_package/build/libs/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
